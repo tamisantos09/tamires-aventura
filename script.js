27aade19e6c92051c9b9dc51cf5b881dd8d3b423
tamisantos09 <aparecida.santos.tamires@escola.pr.gov.br>
2025-09-24 15:04:38 +0000

    sim2

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
- const avanca = document.querySelectorAII(.bnt)
+const avanca = document.querySelectorAll('.btn-proximo');
+avanca.forEach(button => {//para cada clique ele avança
+    button.addEventListener('click',function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-'+ this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+}) 
